e0ae86ad09a6ee1f95abc364434fc653f8c99aac
Arun-cn <arunchandran4321@gmail.com>
2023-10-26 10:30:03 +0000

    add mongodb config

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express =  require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser"); 
 const app =  express();
+const connectToDatabase = require("./utils/db.js");
+
+const db = connectToDatabase();
 
 const port = process.env.PORT || 3000 ;
 
 app.listen(port,(req,res)=>{
     console.log(`server started port: ${port}`);
-})
+});
