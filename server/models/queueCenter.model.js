5a78c2e89867016efebf090d547fb364cb94da26
Arun-cn <arunchandran4321@gmail.com>
2023-11-16 12:18:40 +0000

    add queue center model

---
+++ server/models/queueCenter.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const queueCenterSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    description: {
+        type: String
+    },
+    location: {
+        type: String,
+        trim: true
+    },
+    qeeueCenterImage: {
+        type: String
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    queues: [{
+        type: Schema.Types.ObjectId,
+        ref: "Queue"
+    }],
+    admins: [{
+        type: Schema.Types.ObjectId,
+        ref: "AccessKey"
+    }]
+},{timestamps: true});
+
+const QueueCenter = mongoose.Model('QueueCenter',queueCenterSchema);
+
+export {QueueCenter} ;
