0288ab305003222490953abcc966031926ab30d5
Arun-cn <arunchandran4321@gmail.com>
2023-11-23 06:21:05 +0000

    add hash password method and checks in user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,46 +1,82 @@
-import mongoose, { Schema } from  "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
-     },
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
     firstName: {
       type: String,
       required: true,
       lowercase: true,
       trim: true,
-      index: true
-    },  
+      index: true,
+    },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-      },
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     password: {
-        type: String,
-        required: [true, 'Password is required']
+      type: String,
+      required: [true, "Password is required"],
+    },
+    tokens: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "Token",
       },
-    tokens: [{
-      type: Schema.Type.ObjectId,
-      ref : 'Token'
-    }],
-    queues: [{
-      type: Schema.Type.ObjectId,
-      ref: "Queue"
-    }],
-    accesskeys: [{
-      type: Schema.Type.ObjectId,
-      ref: "AccessKey"  
-    }]
-},{timestamp: true});
+    ],
+    queues: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "Queue",
+      },
+    ],
+    accesskeys: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "AccessKey",
+      },
+    ],
+  },
+  { timestamp: true }
+);
 
-const User = mongoose.Model('User',userSchema);
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-export {User}
+userSchema.methods.isPasswordCorrect = async (password) => {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.createAccessToken = async () => {
+  return await jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const User = mongoose.Model("User", userSchema);
+
+export { User };
