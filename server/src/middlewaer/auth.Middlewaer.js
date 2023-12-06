6da7abbb2a11c38632046d53d1340fdac62ab6f8
Arun-cn <arunchandran4321@gmail.com>
2023-12-06 12:22:33 +0000

    add auth middlewaer

---
+++ server/src/middlewaer/auth.Middlewaer.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/apiError.js";
+
+const requireAuth = async (req, res, next) => {
+  const token = req.headers.autherization;
+
+  if (!token) {
+    const unauthorizedError = new ApiError(
+      401,
+      "Unauthorized - No token provided"
+    );
+    return next(unauthorizedError);
+  }
+
+  try {
+    // Verify the token and extract user information
+    const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    // Attach user information to the request object
+    req.user = {
+      userId: decoded._id,
+      email: decoded.email,
+      username: decoded.username,
+      fullName: decoded.fullName,
+      // Add more user-related information if needed
+    };
+
+    // Proceed to the next middleware or route
+    next();
+  } catch (error) {
+    const unauthorizedError = new ApiError(401, "Unauthorized - Invalid token");
+    return next(unauthorizedError);
+  }
+};
+
+export { requireAuth };
