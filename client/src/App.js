c045dd0e36dccce4c53a8983dbaabc76f16e754e
Arun-cn <arunchandran4321@gmail.com>
2024-01-08 18:58:17 +0530

    add register ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route,Routes } from "react-router-dom"
 import NavBar from "./layout/NavBar/NavBar";
 import Home from "./view/Home/index";
 import Login from "./view/Login/index"
+import Register from "./view/Register/index"
 function App() {
   return (
   <>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<NavBar />} >
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
       </Routes>
    </> 
