7ff44cc2cc9ebc37e28ee12c2345b0ce76c23696
Arun-cn <arunchandran4321@gmail.com>
2024-01-11 22:11:48 +0530

    add privateroute

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
-import { Route,Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./layout/NavBar/NavBar";
 import Home from "./view/Home/index";
-import Login from "./view/Login/index"
-import Register from "./view/Register/index"
+import Login from "./view/Login/index";
+import Register from "./view/Register/index";
+import PrivateRoute from "./routes/PrivateRoute";
+
 function App() {
   return (
-  <>
+    <>
       <Routes>
-        <Route path="/" element={<NavBar />} >
-          <Route index element={<Home />} />
+        <Route path="/" element={<NavBar />}>
+          <Route element={<PrivateRoute />}>
+            <Route index element={<Home />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
       </Routes>
-   </> 
+    </>
   );
 }
 
