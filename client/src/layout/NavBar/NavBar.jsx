1dc26e040bc8b5e4ffa40a1785f4ad477123f64f
Arun-cn <arunchandran4321@gmail.com>
2024-01-11 20:32:28 +0530

    add navigetion function to login button

--- client/src/layout/NavBar/NavBar.jsx
+++ client/src/layout/NavBar/NavBar.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Qick Token</Navbar.Brand>
           <Container className="ms-auto">
-            <Button variant="primary">Login</Button>
-            <Button variant="primary">Register</Button>
+            <Button variant="primary" onClick={handleClick}>
+              Login
+            </Button>
           </Container>
         </Container>
       </Navbar>
