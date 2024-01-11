a7f863322361c6f379a8b54ff51b6254600718b5
Arun-cn <arunchandran4321@gmail.com>
2024-01-11 17:23:50 +0530

    fix login payload error

--- client/src/view/Login/index.js
+++ client/src/view/Login/index.js
@@ -4,7 +4,7 @@ import {useSelector, useDispatch} from "react-redux"
 import { login, logout } from '../../features/auth/auth.Slice';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const {user, isAuthicatted} = useSelector(
@@ -13,31 +13,26 @@ const Login = () => {
 
   const dispatch = useDispatch()
 
-  const HandleLogin = () => {
+  const HandleLogin = (e) => {
+    e.preventDefault()
     const userData = {
-      username,
+      email,
       password
     }
 
     dispatch(login(userData));
-    loguser()
-    console.log(`user: ${user}`)
   };
-
-  const loguser = async () =>{
-    await console.log(user)
-  }
 
   return (
     <div className="container mt-5">
       <Form>
         <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
+          <Form.Label>Email</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
 
