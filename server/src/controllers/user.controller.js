a64280618d33a856246ea7f2e666de7a84908b5a
Arun-cn <arunchandran4321@gmail.com>
2024-02-13 20:02:24 +0530

    add get user's all queue

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,42 +1,58 @@
-import {User} from "../models/user.model.js";
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/apiError.js"
-import {ApiResponse} from "../utils/apiResponse.js"
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { Queue } from "../models/queue.model.js";
 
 const getUserController = asyncHandler(async (req, res) => {
-    const userId = req.user.userId;
- 
-    if (!userId) throw new ApiError(400,"Id required")
+  const userId = req.user.userId;
 
-    const user = User.findById(userId).select("fullName queues");
+  if (!userId) throw new ApiError(400, "Id required");
 
-    if(!user) throw new ApiError(400, "User not found");
+  const user = User.findById(userId).select("fullName queues");
 
-    const response = new ApiResponse(200,{
-        username : username,
-        fullName : user.fullName,
-        queues : user.queues
-    });
+  if (!user) throw new ApiError(400, "User not found");
 
-    res.status(response.statusCode).json(response);
+  const response = new ApiResponse(200, {
+    username: username,
+    fullName: user.fullName,
+    queues: user.queues,
+  });
+
+  res.status(response.statusCode).json(response);
 });
 
 const getUserProfileController = asyncHandler(async (req, res) => {
-    const _Id = req.user.userId;
+  const _Id = req.user.userId;
 
-    if (!_Id) throw new ApiError(400,"Id required")
+  if (!_Id) throw new ApiError(400, "Id required");
 
-    const user = await User.findById(_Id).select("fullName username queues");
+  const user = await User.findById(_Id).select("fullName username queues");
 
-    if(!user) throw new ApiError(400, "User not found");
+  if (!user) throw new ApiError(400, "User not found");
 
-    const response = new ApiResponse(200,{
-        username : user.username,
-        fullName : user.fullName,
-        queues : user.queues
-    });
+  const response = new ApiResponse(200, {
+    username: user.username,
+    fullName: user.fullName,
+    queues: user.queues,
+  });
 
-    res.status(response.statusCode).json(response);
+  res.status(response.statusCode).json(response);
 });
 
-export {getUserController, getUserProfileController}
+const getUserQueues = asyncHandler(async (req, res) => {
+  console.log("starttrd");
+  const userId = req.user.userId;
+  console.log(userId);
+
+  if (!userId) throw new ApiError(400, "user id required");
+
+  const queues = await Queue.find({ "createdBy.creatorId": userId }).exec();
+
+  if (!queues) throw new ApiError(500, "queues not founded");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, queues, "queues feached successfully"));
+});
+
+export { getUserController, getUserProfileController, getUserQueues };
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import {
   getUserController,
   getUserProfileController,
+  getUserQueues,
 } from "../controllers/user.controller.js";
 import { requireAuth } from "../middlewaer/auth.Middlewaer.js";
 
 const router = Router();
 
 router.route("/me").get(requireAuth, getUserProfileController);
+router.route("/queues").get(requireAuth, getUserQueues);
 router.route("/:username").get(getUserController);
 
 export default router;
