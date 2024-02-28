09dbc0f56b1d6ea4f4bf48285196e0053bb02307
Arun-cn <arunchandran4321@gmail.com>
2024-02-28 14:49:45 +0530

    fix token view

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import {configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/auth.Slice"
+import authReducer from "../features/auth/auth.Slice";
+import tokenReducer from "../features/token/token.Slice";
 
 const store = configureStore({
     reducer : {
         auth : authReducer,
+        tokens : tokenReducer
     }
 })
 
--- client/src/view/Tokens/index.jsx
+++ client/src/view/Tokens/index.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React,{ useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux"
 import Card from "../../Components/TokenCard"
-function index() {
+import { fetchTokens } from '../../features/token/token.Service'
+
+function TokensList() { 
+  const dispatch = useDispatch();
+  const tokens = useSelector((state) => state.tokens.tokens);
+
+  useEffect(() => {
+    dispatch(fetchTokens());
+  }, [dispatch]);
+
+  function handileGetTokens () {
+    console.log("gettoken function called")
+    dispatch(fetchTokens())
+  }
+
   return (
     <div>
-      <Card name={"medical queue"} number={"6"} owner={"city hospital"} />
-    </div>
+    <h1>Token Dashboard</h1>
+    {tokens && tokens.length > 0 ? (
+        tokens.map((token) => (
+          <Card
+            key={token.id}
+            name={token.queue}
+            owner={token.owner} // Assuming each token has a unique identifier
+            number={token.tokenNumber}
+          />
+        ))
+      ) : (
+        <div>
+        <p>Loading tokens...</p>
+        <button onClick={handileGetTokens}>load more</button>
+        </div>
+      )}
+
+      </div>
   )
 }
 
-export default index
+export default TokensList
